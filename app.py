a770f7512d9ca5bf54db4717d5776a0823a0e02d
sauraabbhh2004 <141743464+sauraabbhh2004@users.noreply.github.com>
2024-02-22 11:10:21 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}]
+
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', JOB=jobs)
 
 
 if __name__ == '__main__':
